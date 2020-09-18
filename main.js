21b0459c396f3e3b0f6c86b09d3dfb21b4381087
sangjunpark <juntech0421@github.com>
2020-09-18 20:00:50 +0900

    섹션0 스크롤 위치에 따른 스티키 항목들 나타내고 사라ㅈ기

--- main.js
+++ main.js
@@ -11,17 +11,26 @@
       scrollHeight: 0,
       objs: {
         container: document.getElementById("scroll_section_0"),
-        messageOne: document.querySelector('#scroll_section_0 .main_message.one'),
-        messageTwo: document.querySelector('#scroll_section_0 .main_message.two'),
-        messageThree: document.querySelector('#scroll_section_0 .main_message.three'),
-        messageFour: document.querySelector('#scroll_section_0 .main_message.four')
+        messageOne: document.querySelector(
+          "#scroll_section_0 .main_message.one"
+        ),
+        messageTwo: document.querySelector(
+          "#scroll_section_0 .main_message.two"
+        ),
+        messageThree: document.querySelector(
+          "#scroll_section_0 .main_message.three"
+        ),
+        messageFour: document.querySelector(
+          "#scroll_section_0 .main_message.four"
+        ),
       },
       values: {
-        messageOneOpacity: [0, 1, {start: 0.1, end: 0.2}],
-        messageTwoOpacity: [0, 1, {start: 0.3, end: 0.4}],
-        messageThreeOpacity: [0, 1, {start: 0.5, end: 0.6}],
-        messageFourOpacity: [0, 1, {start: 0.7, end: 0.8}]
-      }
+        messageOneOpacity_in: [0, 1, { start: 0.1, end: 0.2 }],
+        messageOneOpacity_out: [1, 0, { start: 0.25, end: 0.3 }],
+        messageTwoOpacity: [0, 1, { start: 0.3, end: 0.4 }],
+        messageThreeOpacity: [0, 1, { start: 0.5, end: 0.6 }],
+        messageFourOpacity: [0, 1, { start: 0.7, end: 0.8 }],
+      },
     },
     {
       //scene id 1
@@ -56,9 +65,7 @@
     // scroll section 높이 세팅
     for (let i = 0; i < sceneInfo.length; i++) {
       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-      sceneInfo[
-        i
-      ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+      sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
 
     yOffset = window.pageYOffset;
@@ -71,56 +78,69 @@
         break;
       }
     }
-    
+
     document.body.setAttribute("id", `show_scene_${currentScene}`);
   }
 
   function calcValues(values, currentYOffset) {
-
     let rv;
     const scrollHeight = sceneInfo[currentScene].scrollHeight;
     let scrollRatio = currentYOffset / scrollHeight;
+
     if (values.length === 3) {
       //start - end 사이에 실행
       const scrollStartPart = values[2].start * scrollHeight;
       const scrollEndPart = values[2].end * scrollHeight;
       const scrollHeightPart = scrollEndPart - scrollStartPart;
-      
-      if (currentYOffset <= scrollEndPart && currentYOffset >= scrollStartPart) {
-        rv = (currentYOffset - scrollStartPart) / scrollHeightPart * (values[1] - values[0]) + values[0]
+
+      if (
+        currentYOffset <= scrollEndPart &&
+        currentYOffset >= scrollStartPart
+      ) {
+        rv =
+          ((currentYOffset - scrollStartPart) / scrollHeightPart) *
+            (values[1] - values[0]) +
+          values[0];
       } else if (currentYOffset < scrollStartPart) {
-        rv = values[0]
+        rv = values[0];
       } else if (currentYOffset > scrollEndPart) {
-        rv = values[1]
+        rv = values[1];
       }
-      
     } else {
       rv = scrollRatio * (values[1] - values[0]) + values[0];
     }
-    
+
     return rv;
   }
 
   function playAnimation() {
     const objs = sceneInfo[currentScene].objs;
     const values = sceneInfo[currentScene].values; //현재 스크롤 섹션의 values 객체
-    let currentYOffset = yOffset - prevScrollHeight;//현재 스크롤 섹션에서 스크롤이 얼마나 됐는지에 대한 값
+    const scrollHeight = sceneInfo[currentScene].scrollHeight;
+    let currentYOffset = yOffset - prevScrollHeight; //현재 스크롤 섹션에서 스크롤이 얼마나 됐는지에 대한 값
+    const scrollRatio = currentYOffset / scrollHeight;
 
-    switch(currentScene) {
+    switch (currentScene) {
       case 0:
-        let messageOneOpacity_in = calcValues(values.messageOneOpacity, currentYOffset)
-
-        objs.messageOne.style.opacity = messageOneOpacity_in
-        
+        let messageOneOpacity_in = calcValues(
+          values.messageOneOpacity_in,
+          currentYOffset
+        );
+        let messageOneOpacity_out = calcValues(
+          values.messageOneOpacity_out,
+          currentYOffset
+        );
+        if (scrollRatio <= 0.25) {
+          objs.messageOne.style.opacity = messageOneOpacity_in;
+        } else {
+          objs.messageOne.style.opacity = messageOneOpacity_out;
+        }
         break;
       case 1:
-
         break;
       case 2:
-        
         break;
       case 3:
-       
         break;
     }
   }
@@ -152,5 +172,5 @@
     yOffset = window.pageYOffset;
     scrollLoop();
   });
-  window.addEventListener('load', setLayout);
+  window.addEventListener("load", setLayout);
 })();
